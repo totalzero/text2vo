be8fd1d8ed32b3c8b26f6008a7a2948b9fe20e32
TotalZero <radek.solekr@icloud.com>
2024-10-27 17:43:00 +0100

    initial commit

---
+++ lib/text2vo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "text2vo/version"
+
+module Text2vo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/text2vo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Text2vo
+  VERSION = "0.1.0"
+end
